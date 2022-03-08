28d849d16bbe7442b1f87a06cd53631582de41b6
KavyaHarigol <98875082+KavyaHarigol@users.noreply.github.com>
2022-03-08 21:59:04 +0530

    Update sub.ino

--- Project/3_Implementation/src/sub.ino
+++ Project/3_Implementation/src/sub.ino
@@ -1,23 +1,23 @@
 void display2()
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);                        // setting  cursor position to top left of LCD
   lcd.print("Train Arrived.");
-  lcd.setCursor(2, 1);
+  lcd.setCursor(2, 1);                        // setting cursor position to 2nd col of 1st row
   lcd.print("Please wait");
-  digitalWrite(Buzzer, HIGH);
-  digitalWrite(led, HIGH);
+  digitalWrite(Buzzer, HIGH);                 // Turn ON buzzer
+  digitalWrite(led, HIGH);                    // Turn On LED
   delay(5000);
 }
 
 void display1()
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);                        // setting  cursor position to top left of LCD
   lcd.print("Train Departured,");
-  lcd.setCursor(2, 1);
+  lcd.setCursor(2, 1);                        // setting cursor position to 2nd col of 1st row
   lcd.print("Gate is open");
-  digitalWrite(Buzzer, LOW);
-  digitalWrite(led, LOW);
+  digitalWrite(Buzzer, LOW);                  // Turn OFF buzzer
+  digitalWrite(led, LOW);                     // Turn OFF LED
   delay(5000);
 }
