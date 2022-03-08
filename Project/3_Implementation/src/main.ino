3df1e65c29c41c576031f674fb9130ef0d4818dc
KavyaHarigol <98875082+KavyaHarigol@users.noreply.github.com>
2022-03-08 22:04:03 +0530

    Update main.ino

--- Project/3_Implementation/src/main.ino
+++ Project/3_Implementation/src/main.ino
@@ -3,14 +3,14 @@
 #define Buzzer 13                                      //buzzer
 #define led 12                                        //led
 
-Servo servoMain;                                     // Define our Servo
+Servo servoMain;                                     // Defining Servo motor
 int trigpin = 10;
 int echopin = 11;
 int distance;
 float duration;
 float cm;
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);              //lcd(RS,EN,D4,D5,D6,D7)
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);             //lcd(RS,EN,D4,D5,D6,D7)
 
 void setup()
 {
@@ -20,7 +20,7 @@ void setup()
     pinMode(Buzzer,OUTPUT);                     // pin 13 as output
     pinMode(led,OUTPUT);                        // pin 12 as putput
     Serial.begin(9600);
-    lcd.begin(16, 2);                           //Lcd intialize
+    lcd.begin(16, 2);                           // set up the lcd 16x2
 }
 
 void loop()
@@ -34,16 +34,16 @@ void loop()
   cm = (duration/58.82);
   distance = cm;
   
-  if(distance<60)
+  if(distance<60)                           // check distance is then than 60cm
   {
     servoMain.write(0);                     // Turn Servo back to center position (clockwise Rotate)
-    display1();
+    display1();                             // go to lcd function
     delay(3000); 
   }
    else
    {
      servoMain.write(90);                   // Turn Servo back to center position (Anticlockwise Rotate)
-     display2();
+     display2();                            // go to lcd function
      delay(3000);
    }   
 }
